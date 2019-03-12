c2c7e1bd2f2a0d7099656bfe26591d724c830e21
David V <david@venin.pro>
2019-03-12 12:32:43 +0100

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import CounterContainer from '../Counter/CounterContainer';
+import Home from '../Home/Home';
 
 const App = () => (
   <Router>
     <div className="container">
       <nav>
+        <Link to="/">Home</Link>
         <Link to="/counter">Counter</Link>
       </nav>
       <div className="App">
+        <Route exact path="/" component={Home} />
         <Route path="/counter" component={CounterContainer} />
       </div>
     </div>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home">
+    <h2>Navigate through your application</h2>
+  </div>
+);
+
+export default Home;
